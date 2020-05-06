2794f9ca17962486910d165d20b57b5428453f16
cncxlinux <64398814+cncxlinux@users.noreply.github.com>
2020-05-06 11:22:34 +0700

    ya
    ya

---
+++ main.py
@@ -0,0 +1,56 @@
+import time
+import os 
+import sys
+
+
+os.system("clear")
+xl = """
+[!] DDOS ATTACK [!]
+[?] BY. xlinux  [?]
+[/] Instagram : @xlinux___  [/]
+[<] Whatsapp : +62895616102309 [>]
+"""
+print(xl)
+print()
+
+start1 =input("Masukan Web nya :") 
+start2 =input("Masukan IP Web nya :") 
+start3 =input("Masukan Port :") 
+start4 =input("Masukan Packet :") 
+print()
+os.system("clear")
+print()
+print("Website :", start1) 
+print("IP Website :", start2)
+print("Port Website :", start3)
+print("Packet Yg Di Kirim :", start4)
+print()
+print()
+Pilih =input("Lanjutkan:[y/n]")
+print()
+if Pilih.lower() == "y":
+    while Pilih.lower() == "y":
+        print('Mengirim Paket Ke', start1)
+        time.sleep(0.20)
+else:
+    if Pilih.lower() == "n":
+        sys.exit("EXIT-_-")
+    else:
+        print("Pilihannya Cuma y dan n Doang tong -_- ")
+        os.system("exit")
+
+
+   
+    
+
+   
+
+
+    
+    
+
+
+
+
+
+
